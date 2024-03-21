e9bb75a3573e83d05fdbe01d681747ff97fb9db4
Jacob Suiter <jasuiter@mtu.edu>
2024-03-21 11:06:10 -0400

    Basics of Attacking, Animation, and the rest of the current framework

---
+++ Bam Bam Pow Pow Basic Scene/entities/entity_base.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var hp: int = 1
+@export var weight: int = 100
+@export var score: int = 0
+
+@onready var sprite = $Sprite2D
+@onready var collShape = $CollisionShape2D
+@onready var animPlayer = $AnimationPlayer
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	animPlayer.play("idle")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta * weight/100
+
+	move_and_slide()
+
+
+func _on_hurtbox_area_entered(hitbox):
+	var base_damage = hitbox.damage
+	self.score += base_damage
+	print(hitbox.get_parent().name + "'s hitbox touched " + name + "'s hurtbox and dealth " + str(base_damage))
---
+++ Bam Bam Pow Pow Basic Scene/entities/newplayer.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+@export var score: int = 0
+
+@onready var animTree : AnimationTree = $AnimationTree
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	animTree.active = true
+	
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed('J')  and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed('P') and is_on_floor():
+		animTree["parameters/conditions/punch"] = true
+	else:
+		animTree["parameters/conditions/punch"] = false
+		
+	if Input.is_action_just_pressed('K') and is_on_floor():
+		animTree["parameters/conditions/kick"] = true
+	else:
+		animTree["parameters/conditions/kick"] = false
+		
+	if Input.is_action_just_pressed('W') and is_on_floor():
+		animTree["parameters/conditions/weapon"] = true
+	else:
+		animTree["parameters/conditions/weapon"] = false
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_hurtbox_area_entered(hitbox):
+	var base_damage = hitbox.damage
+	self.score += base_damage
+	print("Total DMG = " + str(score))
