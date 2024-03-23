194483094237ae35694d0375d4310c0e43382092
Connor Ward <cfw2002@charter.net>
2024-03-23 17:51:00 -0400

    MainMenuTemp

---
+++ Bam Bam Pow Pow Basic Scene/Scenes/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+	SceneSwap.scene_swap("res://main.tscn")
+
+
+func _on_settings_pressed():
+	print("Uncomment and put scene file")
+	#get_tree().change_scene_to_file("PUT FILE HERE")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
