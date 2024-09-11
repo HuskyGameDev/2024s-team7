b602183e4daafd46355cf5ac74cfd0b36077e6bf
Connor Ward <cfw2002@charter.net>
2024-09-11 03:32:07 -0400

    Button Remapping Complete but Ugly
    Should work, pretty sure it works, currently works, please continue working

--- Bam Bam Pow Pow Basic Scene/scripts/Remapping.gd
+++ Bam Bam Pow Pow Basic Scene/scripts/Remapping.gd
@@ -1,33 +1,37 @@
 extends Button
 
 @onready var input_button = $"."
+## Button name correlates to action index value
+## There are 77 god damn built in actions in godot
 
 var is_remapping = false
 var action_to_remap = null
 var remapping_button = null
+var buttonName = null
 var actionList = null
 
 func _ready() -> void:
-	actionList = InputMap.get_actions() #Start at 77
+	buttonName = str($".").split(":")
+	actionList = InputMap.get_actions()
 	_generate()
 
 func _generate():
+	var n = int(buttonName[0])
 	var action_lab = input_button.find_child("ActionL")
 	var input_lab = input_button.find_child("InputL")
-	action_lab.text = actionList[81]
-	var events = InputMap.action_get_events(actionList[81])
+	action_lab.text = actionList[n]
+	var events = InputMap.action_get_events(actionList[n])
 	if events.size() > 0:
 		input_lab.text = events[0].as_text()
 	else:
 		input_lab.text = ""
-	input_button.pressed.connect(_on_input_button_pressed.bind(input_button, 0))
-	print("RAN")
+	input_button.pressed.connect(_on_input_button_pressed.bind(input_button, n))
 	
 	
 func _on_input_button_pressed(button, action):
 		if  !is_remapping:
 			is_remapping = true
-			action_to_remap = action + 81
+			action_to_remap = action
 			remapping_button = button
 			button.find_child("InputL").text = "Press key to rebind..."
 			
