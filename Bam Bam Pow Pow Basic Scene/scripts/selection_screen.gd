83c0a8708d01b9ac2b8cbbde681a3791170ca1b4
gahesse <gahesse@mtu.edu>
2025-02-10 16:57:24 -0500

    Selection Scene
    Created a selection screen for between fights. Contains buttons to access settings, shops, inventory, save, and exit.
    Save is non-functional.
    Temp mspaint art.
    Has coin count visually represented.

---
+++ Bam Bam Pow Pow Basic Scene/scripts/selection_screen.gd
@@ -0,0 +1,43 @@
+extends Node
+
+@onready var coinsPic = $Coins
+@onready var coinsLabel = $coinsLabel
+@onready var coinsCount = ItemStorage.money
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	coinsLabel.text = "Coins: " + str(coinsCount)
+	coinsPic.frame = coinsCount/1000
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_settings_sprite_button_pressed():
+	SceneSwap.scene_swap("res://Scenes/Playable/SettingsMenu.tscn")
+
+
+func _on_item_shop_sprite_button_pressed():
+	SceneSwap.scene_swap("res://Scenes/Playable/ItemShop.tscn")
+
+
+func _on_weapon_shop_sprite_button_pressed():
+	SceneSwap.scene_swap("res://Scenes/Playable/WeaponShop.tscn")
+
+
+func _on_inventory_sprite_button_pressed():
+	SceneSwap.scene_swap("res://Scenes/Playable/EquipScreen.tscn")
+
+
+func _on_exit_sprite_button_pressed():
+	#SceneSwap.scene_swap("")
+	pass
+
+
+func _on_save_sprite_button_pressed():
+	#SceneSwap.scene_swap("")
+	pass
+
+
+func _on_back_to_fight_sprite_button_pressed():
+	SceneSwap.scene_swap("res://Scenes/Playable/Fight.tscn")
