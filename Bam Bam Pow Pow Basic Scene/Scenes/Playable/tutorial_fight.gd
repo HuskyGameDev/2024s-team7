3e3d60a6cd6e951bec0764efef099e5a2f04bc44
raynam40 <raynam@mtu.edu>
2025-02-19 19:08:25 -0500

    Additional changes

--- Bam Bam Pow Pow Basic Scene/Scenes/Playable/tutorial_fight.gd
+++ Bam Bam Pow Pow Basic Scene/Scenes/Playable/tutorial_fight.gd
@@ -21,37 +21,30 @@ func _process(delta):
 		popup.show_message("Move using A and D.")
 		if Input.is_action_just_pressed("R") or Input.is_action_just_pressed("L"):
 			step += 1
-	
 	elif step == 1:
 		popup.show_message("Press J for light attacks.")
 		if Input.is_action_just_pressed("Punch"):
 			step += 1
-	
 	elif step == 2: 
 		popup.show_message("Press K for heavy attacks.")
 		if Input.is_action_just_pressed("Kick"):
 			step += 1
-	
 	elif step == 3:
 		popup.show_message("Press W and J for a high light attack")
 		if Input.is_action_just_pressed("Punch"):
-			step += 1
-		
+			step += 1	
 	elif step == 4:
 		popup.show_message("Press W and K for a high heavy attack")
 		if Input.is_action_just_pressed("Kick"):
 			step += 1
-
 	elif step == 5:
 		popup.show_message("Press S and J for a crouching light attack")
 		if Input.is_action_just_pressed("Punch"):
 			step += 1
-	
 	elif step == 6:
 		popup.show_message("Press S and K for a crouching heavy attack")
 		if Input.is_action_just_pressed("Kick"):
 			step += 1
-	
 	elif step == 7:
 		popup.show_message("Press esc to leave the tutorial")
 
