6ba99f77e0bb01a7f91e947bb2b4e209521adf00
Allen-Wadeen <ajwadeen@mtu.edu>
2025-03-19 20:07:45 -0400

    Revert "Merge branch 'main' into AllenWorkBranch4"
    This reverts commit ce54e5f31d03015760001f1233f6631085d0a358, reversing
    changes made to 9b59d1cf66f4a4d160a2590379808a2b6b127596.

---
+++ Bam Bam Pow Pow Basic Scene/scripts/infinity_fight.gd
@@ -0,0 +1,28 @@
+## This scene handles all changes to FightBase specific to the Infinity mode
+##
+## Infinity is a training mode. In it, you fight an immortal sandbag
+## Player can gain coins for upgrades without Hospital Bills
+
+extends Node
+
+# Node References
+@onready var enemy = $Fight_Base/enemy
+@onready var score_label = $score
+
+func _ready():
+	# Set fight type to Infinity
+	FightDetails.infinity = true
+
+func _process(delta):
+	# Set score value
+	score_label.text = "Score: " + str(enemy.score)
+
+## SceneSwap to Results when timer ends
+func _on_fight_base_timeout():
+	SceneSwap.scene_swap("res://Scenes/Playable/ResultsScreen.tscn") # Swap
+
+# Go to Selection on esc
+func _input(event):
+	if Input.is_action_just_pressed('Esc'):
+		enemy.calc_money()
+		SceneSwap.scene_swap("res://Scenes/Playable/SelectionScreen.tscn")
