54d8742f53e3fc532cc5383f1e18192f5d3006e9
gahesse <gahesse@mtu.edu>
2025-03-27 18:36:37 -0400

    Standard Press Sprite2DButton
    Added a 'press' function that moves one over to the Sprite2DButton script

--- Bam Bam Pow Pow Basic Scene/scripts/sprite_2d_button.gd
+++ Bam Bam Pow Pow Basic Scene/scripts/sprite_2d_button.gd
@@ -10,38 +10,67 @@ extends Sprite2D
 signal SpriteButtonPressed
 signal mouse_entered
 signal mouse_exited
+signal button_down
+signal button_up
+
+@onready var button = $Button
+@onready var hoverDivider = (self.hframes * self.vframes)/ 2
+
+@export var hoverWhiteStandard = false
+@export var pressStandard = false
+@export var tooltip: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Make button resize to Sprite2d given image
-	$Button.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	button.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	button.tooltip_text = tooltip
 
 # Only signal leaving scene, connect to parent scene like any other button press
 func _on_button_pressed():
 	SpriteButtonPressed.emit()	# Emits buttonPressed signal
 	#print("emit")	# Test if button is emitting
 	
-	
 ## Color changes for hover/press using selfLayerEffects shader:
 
 ##-----------
-## Determine if multiply on (button pressed)
+## Determine if pressed, add multiply
 
 func _on_button_button_down():		# Pressed
 	self.material.set_shader_parameter("onoffMult",1)		# Add multiply
-	
+	if pressStandard:
+		pressSprite()
+	button_down.emit()
+
 func _on_button_button_up():		# Removed Press
 	self.material.set_shader_parameter("onoffMult",0)		# Remove multiply
-	
+	if pressStandard:
+		unpressSprite()
+	button_up.emit()
+
+func pressSprite():
+	self.frame = frame + 1
+
+func unpressSprite():
+	self.frame = frame - 1
 
 ##-----------
-## Determine if soft light on (button hover)
+## Determine if hover, add white outline and softlight
+
+func hoverWhite():
+	self.frame = frame + hoverDivider
+
+func removeWhite():
+	self.frame = frame - hoverDivider
 
 func _on_button_mouse_entered():	# Hovering
 	self.material.set_shader_parameter("onoffSoftLight",1)		# Add soft light
+	if hoverWhiteStandard:
+		hoverWhite()
 	emit_signal("mouse_entered")
-	
 
 func _on_button_mouse_exited():		# Removed Hover
 	self.material.set_shader_parameter("onoffSoftLight",0)		# Remove soft light
+	if hoverWhiteStandard:
+		removeWhite()
 	emit_signal("mouse_exited")
