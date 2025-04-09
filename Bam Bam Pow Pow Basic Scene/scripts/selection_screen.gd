431ff40cc6f5d98654106189f4ee339dc8f15267
gahesse <gahesse@mtu.edu>
2025-04-08 23:49:05 -0400

    Animation Progress
    A push to revert back to if I start breaking everything. Created animations in animatedsprite2d for enemies and player shatter. Put player shatter in animationplayer.
    Need to put player shatter in the animation tree. Add shatter state?
    Need to handle the enemy animations with an animation player.

--- Bam Bam Pow Pow Basic Scene/scripts/selection_screen.gd
+++ Bam Bam Pow Pow Basic Scene/scripts/selection_screen.gd
@@ -76,6 +76,7 @@ func _on_save_sprite_button_pressed():
 	ItemStorage.save_game()
 	Dialogic.start('SavedNotification')
 
+#TODO: Replace this back to fight animation handling with a blended animation tree (capture mode)
 func _on_back_to_fight_sprite_button_pressed():
 	if FightDetails.infinity:
 		SceneSwap.scene_swap("res://Scenes/Playable/InfinityFight.tscn")
